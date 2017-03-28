7e5229549575758449199b1d97ca705c51e99281
Josep M Sobrepere <josepot@gmail.com>
2017-03-28 17:41:00 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const getMatcher = (pattern) => {
+  const patternType = typeof pattern;
+  if (patternType === 'string') return ({ type }) => type === pattern;
+  if (patternType === 'function') return pattern;
+  if (Array.isArray(pattern)) {
+    return (action, state) => {
+      let val;
+      for (let i = 0; !val && i < pattern.length; i += 1) {
+        val = getMatcher(pattern[i])(action, state);
+      }
+      return val;
+    };
+  }
+
+  throw new Error(
+    `Watcher pattern was expected to be a string, a function or an Array of patterns, instead it received ${patternType}`
+  );
+};
+
+export default (...args) => {
+  const watchers = args.map(([ pattern ]) => getMatcher(pattern));
+
+  return initialState => (state = initialState, action) => {
+    const winnerIdx = watchers.findIndex(watcher => watcher(action, state));
+
+    return winnerIdx > -1 ?
+      args[winnerIdx][1](state, action) :
+      state;
+  };
+};
