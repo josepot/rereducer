1ec1a6066b518d60ba2a707f469b88f3836fe31f
Josep M Sobrepere <josepot@gmail.com>
2018-09-10 22:25:58 +0200

    v2.1.0: utils/customMemoized

--- src/concatReducer.js
+++ src/concatReducer.js
@@ -1,14 +1,11 @@
-import { flagMemoized, registerExternalReducer } from './utils/index'
+import { customMemoized, registerExternalReducer } from './utils/index'
 
 export default getter_ => {
   const getter = registerExternalReducer(getter_)
-  let prevState, prevVal, prevResult
-  return flagMemoized(function() {
-    const state = arguments[0]
-    const val = getter.apply(null, arguments)
-    if (state === prevState && val === prevVal) return prevResult
-    prevVal = val
-    prevState = state
-    return (prevResult = state.concat(val))
-  })
+  return customMemoized(
+    function() {
+      return [arguments[0], getter.apply(null, arguments)]
+    },
+    (state, val) => state.concat(val)
+  )
 }
--- src/payload.js
+++ src/payload.js
@@ -1,11 +1,6 @@
-import { path, flagMemoized } from './utils/index'
+import { customMemoized, path } from './utils/index'
 
 export default (...args) => {
-  const pathFn = path.bind(path, args)
-  let prevPayload, prevResult
-  return flagMemoized((s, { payload } = {}) => {
-    if (prevPayload === payload) return prevResult
-    prevPayload = payload
-    return (prevResult = pathFn(payload))
-  })
+  const pathFn = path.bind(null, ['payload'].concat(args))
+  return customMemoized((state, action) => [action], pathFn)
 }
