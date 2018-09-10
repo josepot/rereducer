fd7babc1b6c8d99523272ac78753b21183fbd53e
Josep M Sobrepere <josepot@gmail.com>
2018-09-10 22:43:16 +0200

    v2.1.0: concatReducer: basic tests and export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import addReducer from './addReducer'
+import concatReducer from './concatReducer'
 import getPayload from './getPayload'
 import isType from './isType'
 import payload from './payload'
@@ -8,6 +9,7 @@ import subReducer from './subReducer'
 export default rereducer
 export {
   addReducer,
+  concatReducer,
   getPayload, // keep backwards compatibility
   isType,
   payload,
