54d6284f501ce688ce0a617f467bbfe9b563ad27
Josep M Sobrepere <josepot@gmail.com>
2018-10-05 21:41:34 +0200

    [IMPR]: composeReducers extra args

--- src/composeReducers.js
+++ src/composeReducers.js
@@ -3,5 +3,5 @@ import { always } from './utils/index'
 export default function composeReducers(...funcs) {
   if (funcs.length === 0) return always
   if (funcs.length === 1) return funcs[0]
-  return funcs.reduce((a, b) => (...args) => a(b(...args), args[1]))
+  return funcs.reduce((a, b) => (s, ...args) => a(b(s, ...args), ...args))
 }
