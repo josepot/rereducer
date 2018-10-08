35c8d493be052e00609eaa2d2b7308a4d8a4d516
Josep M Sobrepere <josepot@gmail.com>
2018-10-09 01:49:53 +0200

    [IMPR] Improve memoization

--- src/path.js
+++ src/path.js
@@ -1,4 +1,4 @@
-import { assocPath, path, toReducer } from './utils/index'
+import { assocPath, flagMemoized, path, toReducer } from './utils/index'
 import composeReducers from './composeReducers'
 import createReducer from './createReducer'
 import getState from './getState'
@@ -7,7 +7,7 @@ export default isInner => (getters, reducer_) => {
   const reducer = toReducer(reducer_)
   const getCurrentValue = createReducer([getters, getState], path)
   const getNewValue = isInner
-    ? composeReducers(reducer, getCurrentValue)
+    ? flagMemoized(composeReducers(reducer, getCurrentValue))
     : reducer
 
   return createReducer(
