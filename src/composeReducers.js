c31cc1766de11b625c9e0ffd3ed44f30f94f0b5f
Josep M Sobrepere <josepot@gmail.com>
2018-10-09 01:38:19 +0200

    [FIX] composeReducers zero args issue

--- src/composeReducers.js
+++ src/composeReducers.js
@@ -1,7 +1,7 @@
-import { always } from './utils/index'
+import getState from './getState'
 
 export default function composeReducers(...funcs) {
-  if (funcs.length === 0) return always
+  if (funcs.length === 0) return getState
   if (funcs.length === 1) return funcs[0]
   return funcs.reduce((a, b) => (s, ...args) => a(b(s, ...args), ...args))
 }
