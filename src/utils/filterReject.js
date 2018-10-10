a1c2002b02894dddb0b26f7fda608613ae0283d5
Josep M Sobrepere <josepot@gmail.com>
2018-10-10 22:43:05 +0200

    [IMPR] map & filter return same if no changes

--- src/utils/filterReject.js
+++ src/utils/filterReject.js
@@ -1,10 +1,21 @@
+const filterArray = (fn, target) => {
+  const result = target.filter(fn)
+  return result.length === target.length ? target : result
+}
+
+const filterObject = (fn, target) => {
+  const result = {}
+  let changed = false
+  for (let key in target) {
+    const val = target[key]
+    if (fn(val, key, target)) result[key] = val
+    else changed = true
+  }
+  return changed ? result : target
+}
+
 export const filter = (fn, target) =>
-  Array.isArray(target)
-    ? target.filter(fn)
-    : Object.keys(target).reduce((acc, key) => {
-        if (fn(target[key], key, target)) acc[key] = target[key]
-        return acc
-      }, {})
+  (Array.isArray(target) ? filterArray : filterObject)(fn, target)
 
 export const reject = (fn, target) =>
   filter(function() {
