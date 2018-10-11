887c7ab70f91335ec5f32d39293a6ca03f266e44
Josep M Sobrepere <josepot@gmail.com>
2018-10-11 17:27:17 +0200

    todomvc WIP

--- test/todomvc/todos.js
+++ test/todomvc/todos.js
@@ -1,13 +1,12 @@
-import { compose as c, combineReducers } from 'redux'
-import { every, identity, not, prop, propEq, reject } from 'ramda'
+import { compose as c } from 'redux'
+import { every, findIndex, not, prop, propEq, reject } from 'ramda'
 import {
   concatReducer,
   fromAction,
-  innerReducer,
   mapReducer,
+  propReducer,
   rejectReducer,
-  switchReducers,
-  updateReducer
+  switchReducers
 } from '../../src'
 import {
   ADD_TODO,
@@ -19,24 +18,23 @@ import {
 } from './actions'
 
 const isActionId = c(propEq('id'), fromAction(['id']))
-const getNextId = arr => arr.map(prop('id')).reduce(Math.max) + 1
+const getIdxByActionId = c(findIndex, isActionId)
 
-const completeAllMapper = c(
-  innerReducer(['completed']),
-  every(prop('completed'))
-)
-const todo = combineReducers({
-  id: identity,
-  text: switchReducers('', [EDIT_TODO, fromAction(['text'])]),
-  completed: switchReducers(false, [COMPLETE_TODO, not])
-})
+const setTodo = propReducer(getIdxByActionId)
+const setText = c(setTodo, propReducer('text'))
+const setComplete = c(setTodo, propReducer('completed'))
+
+const getNextId = arr => arr.map(prop('id')).reduce(Math.max) + 1
+const isCompleted = propEq('completed', true)
+const areAllCompleted = every(isCompleted)
 
 const initialState = [{ id: 0, text: 'Use Redux', completed: false }]
 export default switchReducers(
   initialState,
   [ADD_TODO, concatReducer({ id: getNextId, text: '', completed: false })],
   [DELETE_TODO, rejectReducer(isActionId)],
-  [CLEAR_COMPLETED, reject(propEq('completed', false))],
-  [COMPLETE_ALL_TODOS, mapReducer(completeAllMapper)],
-  [[EDIT_TODO, COMPLETE_TODO], updateReducer(isActionId, todo)]
+  [CLEAR_COMPLETED, reject(isCompleted)],
+  [COMPLETE_ALL_TODOS, mapReducer(c(setComplete, not, areAllCompleted))],
+  [EDIT_TODO, setText(fromAction(['text']))],
+  [COMPLETE_TODO, setComplete(not)]
 )
