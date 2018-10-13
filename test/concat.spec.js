049088ac58b9ed6171ae70feef6ee7699d291265
Josep M Sobrepere Profitós <josepot@gmail.com>
2018-10-13 14:38:27 +0200

    [TESTS] Improve concat tests

--- test/concat.spec.js
+++ test/concat.spec.js
@@ -2,14 +2,17 @@ import { concat, fromPayload } from '../src'
 
 describe('concat', () => {
   test('concats text', () => {
-    const reducer = concat(fromPayload(['text']))
+    const reducer = concat(fromPayload('text'))
     const output = reducer('foo', { payload: { text: 'bar' } })
     expect(output).toBe('foobar')
   })
 
   test('concats arrays', () => {
-    const reducer = concat(fromPayload(['list']))
-    const output = reducer([0, 1], { payload: { list: [2, 3] } })
-    expect(output).toEqual([0, 1, 2, 3])
+    const reducer = concat(fromPayload('list'))
+    const output1 = reducer([0, 1], { payload: { list: [2, 3] } })
+    expect(output1).toEqual([0, 1, 2, 3])
+
+    const output2 = reducer([0, 1], { payload: { list: 3 } })
+    expect(output2).toEqual([0, 1, 3])
   })
 })
