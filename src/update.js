d6ec1deeb86dc418b8d0f18ed913f3e993deb8ac
Josep M Sobrepere Profitós <josepot@gmail.com>
2018-10-14 21:25:20 +0200

    [IMPR] 'update' allow template reducers

--- src/update.js
+++ src/update.js
@@ -1,3 +1,4 @@
+import { toReducer } from './utils/index'
 import createReducer from './createReducer'
 import getAction from './getAction'
 import map from './map'
@@ -5,11 +6,13 @@ import map from './map'
 export default function update() {
   const whenHOF = arguments[0]
   if (!whenHOF) return update
-  const fn = updater =>
-    map(
+  const fn = updater_ => {
+    const updater = toReducer(updater_)
+    return map(
       createReducer([getAction, whenHOF], (action, whenFn) => innerState =>
         whenFn(innerState) ? updater(innerState, action) : innerState
       )
     )
+  }
   return arguments.length < 2 ? fn : fn(arguments[1])
 }
