733e7f4c4d83d93c00aafd6720a5821568d45c99
Josep M Sobrepere Profitós <josepot@gmail.com>
2018-10-14 20:55:03 +0200

    [TESTS] improve 'merge' tests

--- test/merge.spec.js
+++ test/merge.spec.js
@@ -1,18 +1,26 @@
 import { merge, fromAction } from '../src'
 
 describe('mergeReducer', () => {
+  const inputArray = [1, 2, 3]
+  const inputObject = { foo: 'foo', bar: null }
   test('merges Objects', () => {
     const reducer = merge(fromAction(['payload']))
-    const output = reducer(
-      { foo: 'foo', bar: null },
-      { payload: { bar: 'bar', baz: 'baz' } }
-    )
+    const output = reducer(inputObject, { payload: { bar: 'bar', baz: 'baz' } })
     expect(output).toEqual({ foo: 'foo', bar: 'bar', baz: 'baz' })
   })
 
   test('merges Arrays', () => {
     const reducer = merge(fromAction(['payload']))
-    const output = reducer([1, 2, 3], { payload: [4, 5] })
+    const output = reducer(inputArray, { payload: [4, 5] })
     expect(output).toEqual([4, 5, 3])
+  })
+
+  test('it does not return a new instance an empty object/array', () => {
+    const reducer = merge(fromAction(['payload']))
+    const outputArray = reducer(inputArray, { payload: [] })
+    expect(outputArray).toBe(inputArray)
+
+    const outputObject = reducer(inputObject, { payload: {} })
+    expect(outputObject).toBe(inputObject)
   })
 })
