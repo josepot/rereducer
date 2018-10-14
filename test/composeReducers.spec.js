b63728a8412818b186ce6b6404ab482e41ada9ce
Josep M Sobrepere Profitós <josepot@gmail.com>
2018-10-14 18:34:34 +0200

    [TESTS] improve 'compose' tests

--- test/composeReducers.spec.js
+++ test/composeReducers.spec.js
@@ -1,12 +1,23 @@
-import { composeReducers, concat, fromPayload } from '../src'
+import { composeReducers, concat, fromPayload, getState } from '../src'
 
 describe('composeReducers', () => {
-  test('it works', () => {
+  test('it composes reducers from right to left and it alwasy passes the action as the second parameter', () => {
     const getText = fromPayload(['text'])
     const reducer = composeReducers(concat(getText), concat('bar'))
 
     const result = reducer('foo', { payload: { text: 'baz' } })
     const expectedResult = 'foobarbaz'
     expect(result).toEqual(expectedResult)
+  })
+
+  test('when invoked with 0 arguments it returns the identity function', () => {
+    const reducer = composeReducers()
+    expect(reducer).toBe(getState)
+  })
+
+  test('when invoked with 1 function as a parameter it returns that function', () => {
+    const testFn = () => null;
+    const reducer = composeReducers(testFn)
+    expect(reducer).toBe(testFn)
   })
 })
